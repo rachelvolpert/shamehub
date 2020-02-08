9ac5a3025d3289bf8ba3612e3cc1b0667816de26
rvolpert <rvolpert@cogolabs.com>
2020-02-08 16:49:55 -0500

    adding transaction query

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import os
 import pg8000
+import json
 
 app = Flask(__name__)
 
@@ -23,8 +24,35 @@ def login():
 
 @app.route("/transactions")
 def get_transactions():
-    pass
+    father_list = dbcursor.execute('SELECT t.t_id, t.user_id, u.name AS transactor_name, t.date, t.description, t.category, t.amount, NULL as reactor, NULL as reactor_name, NULL as reaction, c.commentor, uc.name, c.comment_text \
+                                    FROM transactions t \
+                                    JOIN users u ON t.user_id = u.user_id \
+                                    LEFT JOIN comments c ON c.transaction_id = t.t_id \
+                                    LEFT JOIN users uc ON uc.user_id = c.commentor \
+                                    WHERE t.user_id IN (SELECT followee FROM followers WHERE follower = 2) \
+                                        OR t.user_id = 2\
+                                    UNION ALL\
+                                    SELECT t.t_id, t.user_id, u.name AS transactor_name, t.date, t.description, t.category, t.amount, r.reactor, ur.name, r.reaction, NULL, NULL, NULL\
+                                    FROM transactions t \
+                                    JOIN users u ON t.user_id = u.user_id \
+                                    LEFT JOIN reactions r ON r.transaction_id = t.t_id \
+                                    LEFT JOIN users ur ON ur.user_id = r.reactor \
+                                    WHERE t.user_id IN (SELECT followee FROM followers WHERE follower = 2) \
+                                        OR t.user_id = 2\
+                                    ORDER BY t_id').fetchall()
+    big_dict_energy = {}  
 
+    for row in father_list:
+        t_id, user_id, transactor_name, timestamp, description, category, amount, reactor, reactor_name, reaction, commentor, commentor_name, comment_text = row
+        if t_id not in big_dict_energy: 
+            big_dict_energy[t_id] = {"name": transactor_name, "price": float(amount), "timestamp": timestamp, "category": category, "comments": [], "reactions": []}
+        if comment_text:
+            big_dict_energy[t_id]["comments"].append({"commentor_name":commentor_name, "comment_text":comment_text})
+        if reaction:
+            big_dict_energy[t_id]["reactions"].append({"reactor_name":reactor_name, "reaction":reaction})
+        
+    return big_dict_energy
+                   
 
 @app.route("/user")
 def get_user():
