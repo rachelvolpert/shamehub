6ef12cf295fcf5d26ceb30fa14622f078e901b91
alexisdubs <dubs.a@husky.neu.edu>
2020-02-08 13:46:20 -0500

    added pycache to gitignore. changed port.

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import os
 
 app = Flask(__name__)
 
@@ -29,4 +30,5 @@ def search():
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get('PORT', 5000))
+    app.run(port=port)
